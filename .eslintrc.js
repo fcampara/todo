3aaedc417bd727b405d1c3d7f7742a0bfd07b6a1
Felipe <fcamparasilva@gmail.com>
2021-01-12 21:41:39 -0300

    feat(eslint): add eslint to project

---
+++ .eslintrc.js
@@ -0,0 +1,3 @@
+const config = require('@todo/lint')
+
+module.exports = config
--- packages/storybook/src/Button/Button.tsx
+++ packages/storybook/src/Button/Button.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './button.scss';
 import { ButtonProps } from './types';
+
 /**
  * Primary UI component for user interaction
  */
